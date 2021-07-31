39b9a1415146af21898a0e7617693c86c0e79be4
Isah Ibrahim <isahibn08@gmail.com>
2021-07-31 22:37:03 +0100

    created the complete homepage

--- src/components/home/Foot.jsx
+++ src/components/home/Foot.jsx
@@ -2,13 +2,21 @@ const Foot = () => {
     return (
         <div className="flex justify-between items-center h-96">
             <div className="bg-white h-full mx-auto w-full flex justify-evenly items-center align-center">
-                <div className="shadow-quote-shadow h-48 rounded-xl px-5">
-                    <img src="/images/abdulhadi.png" alt="" className=""/>
-                    <p>Abdulhadi Allahnana</p>
+                <div className="flex flex-col justify-center shadow-quote-shadow h-48 rounded-xl px-5">
+                    <img src="/images/abdulhadi.png" alt="" className="w-16 mx-auto static mb-5"/>
+                    <p className="text-blue-500 font-semibold my-2">Abdulhadi Allahnana</p>
+                    <div className="font-thin">
+                        <p >Award Winning</p>
+                        <p >Product Designer</p>
+                    </div>
                 </div>
-                <div className="shadow-quote-shadow h-48 rounded-xl px-5 ">
-                    <img src="/images/isah.png" alt="" className=""/>
-                    <p>Isah Abba Ibrahim</p>
+                <div className="flex flex-col justify-center shadow-quote-shadow h-48 rounded-xl px-5 ">
+                    <img src="/images/isah.png" alt="" className="w-16 mx-auto static mb-5"/>
+                    <p className="text-blue-500 font-semibold my-2">Isah Abba Ibrahim</p>
+                    <div className="font-thin">
+                        <p>Award Winning</p>
+                        <p>Frontend Developer</p>
+                    </div>
                 </div>
             </div>
             <div className="bg-blue-600 h-full w-full text-white mx-auto flex justify-center align-center items-center">
